387b5ab158959b9456ece195c4892514ea373cee
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-08-01 22:45:04 -0300

    Basic working example

--- multi-peer-chat/scripts/index.js
+++ multi-peer-chat/scripts/index.js
@@ -1,7 +1,36 @@
 var $ = require('jquery'),
     SimpleMultiPeer = require('simple-multi-peer');
 
+function wrap() {
+  $('#users').html(Object.keys(this.peers).map(function(user) {
+    return `<div>${user}</div>`;
+  }).join(''));
+}
+
+function message(id, data) {
+  $('#messages').append(id + ': ' + data + '<br>');
+  $('#messages').animate({scrollTop: Infinity});
+}
+
 var Peers = new SimpleMultiPeer({
   server: Environment.SIGNALLER_URL,
   room: 'foobar',
+  callbacks: {
+    connect: wrap,
+    close: wrap,
+    data: message,
+  }
+});
+
+$(document).ready(function() {
+  $('#form-send').on('submit', function(event) {
+    event.preventDefault();
+    if (!$('#text').val()) {
+      return;
+    }
+
+    Peers.send($('#text').val());
+    message(Peers.signaller.id, $('#text').val());
+    $('#text').val('');
+  });
 });
